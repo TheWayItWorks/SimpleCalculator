178b97fc8a9540a8de4c9de6c107b20c64e0b657
Ma][ <testmail@ya.ru>
2022-08-18 22:14:33 +0500

    commit from master2

--- app/src/main/java/com/example/simplecalculator/MainActivity.java
+++ app/src/main/java/com/example/simplecalculator/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {///
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
