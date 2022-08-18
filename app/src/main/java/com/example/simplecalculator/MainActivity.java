ae10725f0688e25a69085750ead5ade49aa64f6d
Ma][ <testmail@ya.ru>
2022-08-18 21:14:11 +0500

    improve btnSetListener on feature

--- app/src/main/java/com/example/simplecalculator/MainActivity.java
+++ app/src/main/java/com/example/simplecalculator/MainActivity.java
@@ -13,11 +13,7 @@ import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     TextView tvSolution, tvResult;
-    Button buttonC, buttonOpenBr,buttonCloseBr,buttonDivide;
-    Button button7,button8,button9,buttonMultiply;
-    Button button4,button5, button6,buttonADD;
-    Button button1, button2, button3,buttonMinus;
-    Button buttonAC, button0, buttonDOT,buttonEquals;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,32 +23,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         tvSolution = findViewById(R.id.solution_tv);
         tvResult = findViewById(R.id.result_tv);
 
-        btnSetListener(buttonC, R.id.button_c);
-        btnSetListener(buttonOpenBr, R.id.button_open_bracket);
-        btnSetListener(buttonCloseBr, R.id.button_close_bracket);
-        btnSetListener(buttonDivide, R.id.button_divide);
-        btnSetListener(button7, R.id.button_7);
-        btnSetListener(button8, R.id.button_8);
-        btnSetListener(button9, R.id.button_9);
-        btnSetListener(buttonMultiply, R.id.button_multiply);
-        btnSetListener(button4, R.id.button_4);
-        btnSetListener(button5, R.id.button_5);
-        btnSetListener(button6, R.id.button_6);
-        btnSetListener(buttonADD, R.id.button_add);
-        btnSetListener(button1, R.id.button_1);
-        btnSetListener(button2, R.id.button_2);
-        btnSetListener(button3, R.id.button_3);
-        btnSetListener(buttonMinus, R.id.button_subtraction);
-        btnSetListener(buttonAC, R.id.button_ac);
-        btnSetListener(button0, R.id.button_zero);
-        btnSetListener(buttonEquals, R.id.button_equals);
-        btnSetListener(buttonDOT, R.id.button_dot);
+        btnSetListener(R.id.button_c);
+        btnSetListener(R.id.button_open_bracket);
+        btnSetListener(R.id.button_close_bracket);
+        btnSetListener(R.id.button_divide);
+        btnSetListener(R.id.button_7);
+        btnSetListener(R.id.button_8);
+        btnSetListener(R.id.button_9);
+        btnSetListener(R.id.button_multiply);
+        btnSetListener(R.id.button_4);
+        btnSetListener(R.id.button_5);
+        btnSetListener(R.id.button_6);
+        btnSetListener(R.id.button_add);
+        btnSetListener(R.id.button_1);
+        btnSetListener(R.id.button_2);
+        btnSetListener(R.id.button_3);
+        btnSetListener(R.id.button_subtraction);
+        btnSetListener(R.id.button_ac);
+        btnSetListener(R.id.button_zero);
+        btnSetListener(R.id.button_equals);
+        btnSetListener(R.id.button_dot);
 
 
     }
 
-    private void btnSetListener(Button btn, int id){
-        btn = findViewById(id);
+    private void btnSetListener(int id){
+        Button btn = findViewById(id);
         btn.setOnClickListener(this);
 
     }
